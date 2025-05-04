53eb6e8efadde8af1353d37d35adf663e7e51795
Mesbahul Islam <mesbahultahiat@gmail.com>
2025-05-04 17:44:39 +0300

    added updated info and changed some styles for schedule

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <div className="bg-black py-16 text-center">
       <h1 className="head" data-text="LaunchX">LaunchX</h1>
-      <p className="sub1" data-text="OuluES">OuluES</p>
+      <p className="sub1" data-text="Empowering Innovation Under the Midnight Sun">Empowering Innovation Under the Midnight Sun</p>
     </div>
   );
 }
